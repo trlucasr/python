f36100e1a7edbe50a31cf5fcaca5cf06290f5b45
trlucasr <tr.lucas@live.com>
2021-11-18 08:38:26 -0300

    Atualização 1168

--- 1168 - Intervalo de Primos/1168.py
+++ 1168 - Intervalo de Primos/1168.py
@@ -1,22 +1,32 @@
-inicio   = int(input())
-fim      = int(input())
-contador = 0
+# inicio   = int(input())
+# fim      = int(input())
+# contador = 0
 
-def validaprimo(num):
-  if num < 2:
-    return False 
-  elif num == 2:
-    print(num)
-    return True 
-  else:
-    for a in range(2, num):
-      if num % a == 0:
-        return False
-    print(num)
-    return True
+# def validaprimo(num):
+#   if num < 2:
+#     return False 
+#   elif num == 2:
+#     print(num)
+#     return True 
+#   else:
+#     for a in range(2, num):
+#       if num % a == 0:
+#         return False
+#     print(num)
+#     return True
 
-for a in range(inicio, fim + 1):
-    if validaprimo(a):
-      contador += 1
+# for a in range(inicio, fim + 1):
+#     if validaprimo(a):
+#       contador += 1
 
-print('primos:', contador)
+# print('primos:', contador)
+
+
+num1 = 3
+num2 = 2
+
+def somar(n1,n2):
+  soma=n1+n2
+  return soma
+
+x=2**3-somar(num1,num2)
